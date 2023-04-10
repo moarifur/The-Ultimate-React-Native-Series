ab8c3e9935f0473f4459afab3c22c9ccf337715c
arif <80209693+moarifur@users.noreply.github.com>
2023-04-10 11:46:18 +0600

    React Native -> Fundamentals

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from "react-native-web";
+import {Dimensions} from "react-native";
+
+export default function App() {
+  return (
+    <View>
+      <Text>
+          The Ultimate React Native Series: Fundamentals
+      </Text>
+    </View>
+  );
+}
+
